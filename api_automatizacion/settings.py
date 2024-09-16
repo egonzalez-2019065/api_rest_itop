8763840ff4a70902498e7843c82a810ea5ff8c3c
Alexander González <egonzalez-2019065@kinal.edu.gt>
2024-09-16 16:24:53 -0600

    Cambio de la zona horaria en las configuraciones

--- api_automatizacion/settings.py
+++ api_automatizacion/settings.py
@@ -124,12 +124,10 @@ AUTH_PASSWORD_VALIDATORS = [
 # https://docs.djangoproject.com/en/5.1/topics/i18n/
 
 LANGUAGE_CODE = 'en-us'
-
-TIME_ZONE = 'UTC'
-
+TIME_ZONE = 'America/Guatemala'
+USE_TZ = True
 USE_I18N = True
 
-USE_TZ = True
 
 
 # Static files (CSS, JavaScript, Images)
