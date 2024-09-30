e30aad397e082188ac9b5d5add957a9690c2c124
Alexander González <egonzalez-2019065@kinal.edu.gt>
2024-09-30 11:45:32 -0600

    Agregar la data en el servicio

---
+++ api_rest/tasks/task_insert_data.py
@@ -0,0 +1,220 @@
+
+
+import base64
+import json
+import logging
+import os
+import re
+from urllib.parse import urlencode
+import requests
+
+from api_rest.models import Computer, HistorialComputer, SerialAndIDItop
+
+# Configuración para los logs
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
+
+
+def look(sn):
+    url = os.getenv('ITOP_URL') 
+    us = os.getenv('USER_ITOP')
+    pas = os.getenv('PASSWORD_ITOP') 
+
+    # Codificación de las credenciales
+    credentials = f'{us}:{pas}'
+    encoded_credentials = base64.b64encode(credentials.encode()).decode()
+
+    # Preparación de los headers para la petición
+    headers = {
+        'Content-Type': 'application/json',
+        'Authorization': f'Basic {encoded_credentials}'
+    }
+
+    # Petición para saber si existe en el servicio
+    simple_data = {
+        "operation": "core/get",
+        "class": "PC",
+        "key": f"SELECT PC WHERE serialnumber = '{sn}'",
+        "output_fields": "name"
+    }
+
+    # Convertirlo a JSON  
+    json_data = json.dumps(simple_data)
+
+    # Preparando la URL para la solicitud 
+    encoded_json_data = urlencode({'json_data': json_data}) 
+    url_base = f"{url}&{encoded_json_data}"
+    
+    # Solicitud al API  
+    response = requests.request("POST", url_base, headers=headers)
+    logger.info(response.text)
+
+    # Convirtiendo en json la respuesta
+    response_json = response.json()
+    if response.status_code == 200: 
+        if response_json['code'] == 0:
+            if response_json['message'] != "Found: 1":
+                return False
+            else:
+                return True
+
+
+def insert(): 
+        # Obtener todas las computadoras
+        computers = Computer.objects.all()
+
+        if not computers.exists():
+           logger.error('No hay computadoras para procesar')
+
+        for computer in computers:
+            # Preparando los datos extraídos
+            fields = {
+                    "name": computer.name,
+                    "description": computer.description,
+                    "org_id": computer.organization_id or 15,
+                    "move2production": computer.move2production.strftime("%Y-%m-%d") if computer.move2production else None,
+                    "serialnumber": computer.serialnumber,
+                    "location_id": computer.location_id or 8,
+                    "status": computer.status,
+                    "brand_id": computer.brand_id or 104,
+                    "model_id": computer.model_id or 105,
+                    "purchase_date": computer.purchase_date.strftime("%Y-%m-%d") if computer.purchase_date else None,
+                    "end_of_warranty": computer.end_of_warranty.strftime("%Y-%m-%d") if computer.end_of_warranty else None,
+                    "osfamily_id": computer.osfamily_id or 106,
+                    "osversion_id": computer.os_version_id or 107,
+                    "cpu": computer.cpu,
+                    "ram": computer.ram,
+                    "type": computer.type
+                }
+            # Decidiendo si actualizará o agregará la computadora
+            exist = look(computer.serialnumber)
+            data = None
+            if exist:
+                data = {
+                    "operation": "core/update",
+                    "class": "PC",
+                    "comment": "Computadora actualizada desde el API",
+                    "key": f"SELECT PC WHERE serialnumber = '{computer.serialnumber}'"
+                }
+            else: 
+                data = {
+                    "operation": "core/create",
+                    "class": "PC",
+                    "comment": "Computadora agregada desde el API",  
+                    "output_fields": "name"
+                }
+            
+            data["fields"] = fields
+        
+            # Convertirlo a JSON
+            json_data = json.dumps(data)
+
+            # Preparando la URL  
+            encoded_json_data = urlencode({'json_data': json_data})
+
+            # Preparación de las credenciales para agregarlas a los headers
+            itop_url = os.getenv('ITOP_URL')
+            username = os.getenv('USER_ITOP')
+            password = os.getenv('PASSWORD_ITOP')
+
+            # Codificación de las credenciales
+            credentials = f'{username}:{password}'
+            encoded_credentials = base64.b64encode(credentials.encode()).decode()
+
+            # Preparación de los headers para la petición
+            headers = {
+                'Content-Type': 'application/json',
+                'Authorization': f'Basic {encoded_credentials}'
+            }
+
+            # Enviar la url completa 
+            url_base = f"{itop_url}&{encoded_json_data}"
+            
+            # Enviar la petición a iTop
+            try:
+                response = requests.request("POST", url_base, headers=headers)
+                print(f'Respuesta Itop: {response.text}')
+
+                # Convierte la respuesta a un json
+                itop_response = response.json()
+
+                # Validación de la respuesta general de Itop
+                if response.status_code == 200:
+
+                    # Guardar la computadora con el ID que devuelva Itop
+                    # Convetir el diccionario a cadena de texto
+                    itop_response_str = str(itop_response)
+
+                    # Buscarlo en la cadena de texto
+                    itop_id_match = re.search(r'PC::(\d+)', itop_response_str)
+
+                    # ID extraído
+                    itop_id_final = itop_id_match.group(1)
+
+                    # Guarda el id de la respuesta de Itop y el número de serie
+                    serial_number_instance, created = SerialAndIDItop.objects.get_or_create(
+                        serial_number=computer.serialnumber,  # Buscar por el número de serie
+                        defaults = {
+                            'id_itop': itop_id_final  # Si no existe, crear con este id_itop
+                        }
+                    )
+
+                    # Validación más detallada según el error que devuelva Itop
+                    if itop_response['code'] == 0: 
+
+                        # Procesar respuesta exitosa
+                        historial_computer, created = HistorialComputer.objects.get_or_create(
+                            serialnumber=computer.serialnumber,
+                            defaults={
+                                'name': computer.name,
+                                'organization_id': computer.organization_id,
+                                'location_id': computer.location_id,
+                                'brand_id': computer.brand_id,
+                                'model_id': computer.model_id,
+                                'osfamily_id': computer.osfamily_id,
+                                'type': computer.type,
+                                'cpu': computer.cpu,
+                                'os_version_id': computer.os_version_id,
+                                'status': computer.status,
+                                'ram': computer.ram,
+                                'description': computer.description,
+                                'move2production': computer.move2production,
+                                'purchase_date': computer.purchase_date,
+                                'end_of_warranty': computer.end_of_warranty,
+                            })
+                        if not created:
+                            historial_computer.name = computer.name
+                            historial_computer.organization_id = computer.organization_id
+                            historial_computer.location_id = computer.location_id
+                            historial_computer.brand_id = computer.brand_id
+                            historial_computer.model_id = computer.model_id
+                            historial_computer.osfamily_id = computer.osfamily_id
+                            historial_computer.type = computer.type
+                            historial_computer.cpu = computer.cpu
+                            historial_computer.os_version_id = computer.os_version_id
+                            historial_computer.status = computer.status
+                            historial_computer.ram = computer.ram
+                            historial_computer.description = computer.description
+                            historial_computer.move2production = computer.move2production
+                            historial_computer.purchase_date = computer.purchase_date
+                            historial_computer.end_of_warranty = computer.end_of_warranty
+                            historial_computer.save()
+
+                        # Eliminar la computadora procesada
+                        computer.delete()
+                        print(itop_response_str)
+                        if 'created' in itop_response_str:
+                            logger.info(f"Computadora {computer.serialnumber} fue agregada correctamente")
+                        else:
+                            logger.info(f"Computadora {computer.serialnumber} fue actualizada correctamente")
+
+                    else:
+                        logger.error(f"Error al procesar {computer.serialnumber}, {itop_response['message']} código: {itop_response['code']}")
+                else: 
+                    logger.error(f"Error al procesar {computer.serialnumber}, {response.text} código: {response.status_code}")
+            except requests.RequestException as req_err:
+                logger.error(f"Error al realizar la petición para {computer.serialnumber}: {req_err}")
+            except Exception as e:
+                logger.error(f"Error fatal para {computer.serialnumber}: {e}, la API responde: {itop_response['message']}")   
+
+
