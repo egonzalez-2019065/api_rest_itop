c9641cf468bb685cf9432bb42129642c38d6abf5
Alexander González <egonzalez-2019065@kinal.edu.gt>
2024-10-04 10:50:44 -0600

    Configuración de la primer tarea, limpiar la data

--- api_rest/scheduler_config.py
+++ api_rest/scheduler_config.py
@@ -1,38 +1,36 @@
 # api_rest/scheduler_config.py
 import logging
-from apscheduler.schedulers.background import BackgroundScheduler
+from apscheduler.schedulers.blocking import BlockingScheduler
 from django_apscheduler.jobstores import DjangoJobStore
-from apscheduler.triggers.interval import IntervalTrigger
+from apscheduler.triggers.cron import CronTrigger
 from .tasks.task_clean_data import clear
 
 # Configuración para los logs
-logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
 
-# Crear y configurar el scheduler
-scheduler = BackgroundScheduler()
-scheduler.add_jobstore(DjangoJobStore(), "default")
 
 def configure_scheduler():
+    # Crear y configurar el scheduler
+    scheduler = BlockingScheduler()
+    scheduler.add_jobstore(DjangoJobStore(), "default")
     logger.info("Intentando agregar job 'data' al scheduler.")
+
     if not scheduler.get_job('data'):
         try:
             scheduler.add_job(
                 clear,
-                IntervalTrigger(seconds=10),
+                CronTrigger(second=10),
                 id='data',
                 max_instances=1,
                 coalesce=True
             )
-            logger.info("Job 'data-clear' agregado al scheduler.")
+            logger.info("Job 'data' agregado al scheduler.")
         except Exception as e:
             logger.error(f"Error al agregar el job al scheduler: {e}")
     else:
-        logger.info("Job 'data-clear' ya fue agregado al scheduler.")
+        logger.info("Job 'data' ya fue agregado al scheduler.")
 
-try:
-    scheduler.start()
-    logger.info("Scheduler iniciado.")
-except Exception as e:
-    logger.error(f"Error al iniciar el scheduler: {e}")
-    
+    try:
+        scheduler.start()
+    except Exception as e:
+        logger.error(f"Error al iniciar el scheduler: {e}")
