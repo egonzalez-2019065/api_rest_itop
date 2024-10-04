ef9f9a80e47aa26df7e0b89c5e78c34f796aef73
Alexander González <egonzalez-2019065@kinal.edu.gt>
2024-10-04 14:47:06 -0600

    Se implementa la tarea para mandar los datos al servicio

--- api_rest/scheduler_config.py
+++ api_rest/scheduler_config.py
@@ -2,8 +2,10 @@
 import logging
 from apscheduler.schedulers.blocking import BlockingScheduler
 from django_apscheduler.jobstores import DjangoJobStore
-from apscheduler.triggers.cron import CronTrigger
+from apscheduler.triggers.interval import IntervalTrigger
+from django_apscheduler.models import DjangoJob
 from .tasks.task_clean_data import clear
+from .tasks.task_insert_data import insert
 
 # Configuración para los logs
 logger = logging.getLogger(__name__)
@@ -13,22 +15,41 @@ def configure_scheduler():
     # Crear y configurar el scheduler
     scheduler = BlockingScheduler()
     scheduler.add_jobstore(DjangoJobStore(), "default")
-    logger.info("Intentando agregar job 'data' al scheduler.")
+    logger.info("Inicia el proceso para ejecutar los trabajos con apscheduler")
 
-    if not scheduler.get_job('data'):
+
+    # Elimina las tareas si existe en la BD, para evitar error de duplicidad de tareas
+    if DjangoJob.objects.filter(id='data').exists():
+        logger.info("El trabajo 'data' existe en la base de datos, será eliminado.")
+        DjangoJob.objects.filter(id='data').delete()
+
+    if DjangoJob.objects.filter(id='insert').exists():
+        logger.info("El trabajo 'insert' existe en la base de datos, será eliminado.")
+        DjangoJob.objects.filter(id='insert').delete()
+
+    if not scheduler.get_job('data') and not scheduler.get_job('insert'):
         try:
             scheduler.add_job(
                 clear,
-                CronTrigger(second=10),
+                IntervalTrigger(seconds=10),
                 id='data',
                 max_instances=1,
                 coalesce=True
             )
-            logger.info("Job 'data' agregado al scheduler.")
+
+            scheduler.add_job(
+                insert,
+                IntervalTrigger(seconds=30),
+                id='insert',
+                max_instances=1,
+                coalesce=True
+            )
+
+            logger.info("Tareas creadas exitosamente")
         except Exception as e:
-            logger.error(f"Error al agregar el job al scheduler: {e}")
+            logger.error(f"Error al crear las tareas: {e}")
     else:
-        logger.info("Job 'data' ya fue agregado al scheduler.")
+        logger.info("Los trabajos ya fueron agregados")
 
     try:
         scheduler.start()
