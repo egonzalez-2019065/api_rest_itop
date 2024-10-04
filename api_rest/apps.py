1ecfe3d452da2fa079fba05a06b3e00e082f5f13
Alexander González <egonzalez-2019065@kinal.edu.gt>
2024-10-04 10:50:23 -0600

    Nuevo enfoque para ejecutar la tarea

--- api_rest/apps.py
+++ api_rest/apps.py
@@ -2,8 +2,4 @@ from django.apps import AppConfig
 
 class ApiRestConfig(AppConfig):
     name = 'api_rest'
-
-    def ready(self):
-        from .scheduler_config import configure_scheduler
-         # Llama a la función para configurar el scheduler
-        configure_scheduler()
+        
